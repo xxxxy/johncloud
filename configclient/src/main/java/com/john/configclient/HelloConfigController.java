c22ee5864cc6f8c68eaafffc6006af9c2d80b9fa
ex_huangjy5 <jingyang1.huang@partner.midea.com>
2020-04-13 21:33:08 +0800

    add Config client

---
+++ configclient/src/main/java/com/john/configclient/HelloConfigController.java
@@ -0,0 +1,30 @@
+/**
+ * <html>
+ * <body>
+ * <p> All rights reserved.</p>
+ * <p> Created on 2020/4/13</p>
+ * <p> Created by huangjy</p>
+ * </body>
+ * </html>
+ */
+package com.john.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author huangjy
+ * @since v1.0
+ */
+@RestController
+public class HelloConfigController {
+    @Value("${john}")
+    private String john;
+
+    @RequestMapping("/helloConfig")
+    public String helloConfig(){
+        return john;
+    }
+}
